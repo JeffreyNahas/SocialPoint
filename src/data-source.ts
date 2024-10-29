2692c47f8b9ab5405ac1f607ccb88af250b03409
JeffreyNahas <jeffrey.nahas@mail.mcgill.ca>
2024-10-28 20:32:17 -0400

    Fixed Errors related to database connection

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,23 @@
 import { DataSource } from 'typeorm';
+import { UserAccount } from './main/eventmanagement/model/UserAccount';
 import { User } from './main/eventmanagement/model/User';
 import { Event } from './main/eventmanagement/model/Event';
 import { Venue } from './main/eventmanagement/model/Venue';
+import { Review } from './main/eventmanagement/model/Review'; 
+import { Reply } from './main/eventmanagement/model/Reply';
+import { Notification } from './main/eventmanagement/model/Notification';
+import { UserEventRole } from './main/eventmanagement/model/UserEventRole';
+
 // Add other entities as needed
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'mcgill1204',
+  database: 'event_management',
   synchronize: true, // Set to false in production
   logging: true,
-  entities: [User, Event, Venue], // Add all your models here
+  entities: [User, UserAccount, Event, Venue, Review, Reply, Notification, UserEventRole], // Add all your models here
 });
